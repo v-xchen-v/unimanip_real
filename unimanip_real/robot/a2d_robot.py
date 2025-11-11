f02387c7664d8ac438130196d8ea9e72c46b58c9
Xi Chen <407081305@qq.com>
2025-11-12 00:14:36 +0800

    temp

---
+++ unimanip_real/robot/a2d_robot.py
@@ -0,0 +1,8 @@
+# from .base_robot import BaseRobotSDK
+
+# class A2DRobotSDK(BaseRobotSDK):
+#     """
+#     A2D Robot SDK implementation using the a2d_sdk.robot.RobotDds API.
+    
+#     This implementation uses the actual A2D robot SDK for controlling the humanoid robot.
+#     """
